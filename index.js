80c8b4a3e96434e0d48204d6da9e7da38a734d6e
David <david.reyes@enti.cat>
2025-06-12 20:46:30 +0200

    push final?
    klk

--- index.js
+++ index.js
@@ -1,130 +1,84 @@
-
-
-
-
-
-const PORT=7777;
-
+const PORT = 7777;
 let http = require('http');
 let static = require('node-static');
 let ws = require('ws');
- 
+
 let file = new static.Server('./public');
- 
 let http_server = http.createServer(function (request, response) {
-    request.addListener('end', function () {
-        file.serve(request, response);
-    }).resume();
+  request.addListener('end', function () {
+    file.serve(request, response);
+  }).resume();
 }).listen(PORT);
 
+let ws_server = new ws.Server({ server: http_server });
 
-let ws_server = new ws.Server({server: http_server});
+let player1 = null, player2 = null;
+let spectators = [];
 
+function broadcastToSpectators(info) {
+  spectators.forEach(s => {
+    if (s.readyState === ws.OPEN) s.send(JSON.stringify(info));
+  });
+}
 
-let player1, player2;
-
+function sendToAllPlayersAndSpectators(info) {
+  let json = JSON.stringify(info);
+  if (player1 && player1.readyState === ws.OPEN) player1.send(json);
+  if (player2 && player2.readyState === ws.OPEN) player2.send(json);
+  broadcastToSpectators(info);
+}
 
 ws_server.on('connection', function (conn) {
-
-	console.log("Usuario conectado");
-
-	if (player1 == null){
-		player1 = conn;
-
-		let info = {
-			player_num: 1
-		};
-
-		player1.send( JSON.stringify(info)  );
-
-		player1.on('close', function () {
-			console.log("Player 1 disconnected");
-			player1 = null;
-		});
-
-
-		player1.on('message', function (msg) {
-			if (player2 == null)
-				return;
-
-			let info = JSON.parse(msg);
-
-			if (info.y != null){
-				player2.send( JSON.stringify(info) );
-			}
-			else if (info.by != null) {
-				player2.send( JSON.stringify(info) );
-			}
-			else if (info.score1 != null) {
-
-				player2.send( JSON.stringify(info) );
-				
-				if (info.score1 >= 3 || info.score2 >= 3){
-					
-					let data = {
-						game_over: true,
-						winner: 0
-					};
-
-					if (info.score1 >= 3)
-						data.winner = 1;
-					else
-						data.winner = 2;
-
-					let data_json = JSON.stringify(data); 
-
-					player1.send( data_json );
-					player2.send( data_json );
-
-					return;
-				}
-
-			}
-		});
-	}
-	else if (player2 == null) {
-		player2 = conn;
-
-		let info = {
-			player_num: 2
-		};
-
-		player2.send( JSON.stringify(info)  );
-
-		player2.on('close', function () {
-			console.log("Player 2 disconnected");
-			player2 = null;
-		});
-
-
-
-
-		setTimeout(function() {
-			let info = {
-				game_start: true
-			};
-
-			let info_json = JSON.stringify(info); 
-
-			player1.send( info_json );
-			player2.send( info_json );
-		}, 500);
-
-
-
-
-
-		player2.on('message', function (msg) {
-			if (player1 == null)
-				return;
-		
-			let info = JSON.parse(msg);
-
-			if (info.y != null){
-				player1.send( JSON.stringify(info) );
-			}
-
-		//	console.log("Jugador 2: "+msg);
-		});
-	}
+  if (player1 == null) {
+    player1 = conn;
+    conn.send(JSON.stringify({ player_num: 1 }));
+    conn.on('message', function (msg) {
+      if (player2 == null) return;
+      let info = JSON.parse(msg);
+      if (info.y != null || info.by != null) {
+        if (player2.readyState === ws.OPEN) player2.send(JSON.stringify(info));
+        broadcastToSpectators(info);
+      } else if (info.score1 != null) {
+        if (player2.readyState === ws.OPEN) player2.send(JSON.stringify(info));
+        broadcastToSpectators(info);
+        if (info.score1 >= 3 || info.score2 >= 3) {
+          let data = { game_over: true, winner: info.score1 >= 3 ? 1 : 2 };
+          sendToAllPlayersAndSpectators(data);
+        }
+      }
+    });
+    conn.on('close', () => {
+      player1 = null;
+      let info = { player_left: 1 };
+      if (player2 && player2.readyState === ws.OPEN) player2.send(JSON.stringify(info));
+      broadcastToSpectators(info);
+    });
+  } else if (player2 == null) {
+    player2 = conn;
+    conn.send(JSON.stringify({ player_num: 2 }));
+    setTimeout(() => {
+      let info = { game_start: true };
+      sendToAllPlayersAndSpectators(info);
+    }, 500);
+    conn.on('message', function (msg) {
+      if (player1 == null) return;
+      let info = JSON.parse(msg);
+      if (info.y != null) {
+        if (player1.readyState === ws.OPEN) player1.send(JSON.stringify(info));
+        broadcastToSpectators(info);
+      }
+    });
+    conn.on('close', () => {
+      player2 = null;
+      let info = { player_left: 2 };
+      if (player1 && player1.readyState === ws.OPEN) player1.send(JSON.stringify(info));
+      broadcastToSpectators(info);
+    });
+  } else {
+    spectators.push(conn);
+    conn.send(JSON.stringify({ spectator: true }));
+    conn.on('close', () => {
+      spectators = spectators.filter(s => s !== conn);
+    });
+  }
 });
